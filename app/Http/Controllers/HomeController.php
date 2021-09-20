af2f84c0f8a958bf79c623cc5f6ad5699e3f8a0b
KaiH <kaihoang@gmail.com>
2021-09-20 11:07:16 +0200

    new json

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,26 +26,27 @@ class HomeController extends Controller
 
     public function test(Request $request)
     {
-        print_r('hoi');
+        $file = 'test.pdf';
 
         $parser = new Parser();
-        $pdf = $parser->parseFile('test.pdf');
+        $pdf = $parser->parseFile($file);
 
-//        echo $pdf->getText();
-//        print_r($pdf->getPages());
+        // get full text
+        echo $pdf->getText();
+        print_r($pdf->getPages());
+        echo '<br /><br />';
 
+        // get text per page
         $pages  = $pdf->getPages();
-
         foreach ($pages as $page) {
             echo '---------------------------------<br />';
             echo $page->getText();
         }
-
         echo '<br /><br />';
-        // Retrieve all details from the pdf file.
-        $details  = $pdf->getDetails();
 
-// Loop over each property to extract values (string or array).
+        // get details
+        $details  = $pdf->getDetails();
+        // Loop over each property to extract values (string or array).
         foreach ($details as $property => $value) {
             if (is_array($value)) {
                 $value = implode(', ', $value);
@@ -53,29 +54,48 @@ class HomeController extends Controller
             echo $property . ' => ' . $value . "\n";
         }
 
+        // echte flow
 //        $rawData = $this->grabGovernmentPdfData->grab($pdf);
 //        print_r($rawData);
 //        $payload = $this->payloadTransformer->transform($rawData);
 
-        // insert into cats
-        $output = $this->catsApiClient->addJob(' {
-            "and": [{
-                "field": "is_published",
-                "filter": "exactly",
-                "value": true
-            }, {
-                "field": "status_id",
-                "filter": "exactly",
-                "value": "185291"
-            }
-            ]
+        // insert new job into cats
+        $output = $this->catsApiClient->addJob('{
+           "title":"Chief Operations Technician",
+           "location":{
+              "city":"Wittingland",
+              "state":"IN",
+              "postal_code":"30349-0254"
+           },
+           "country_code":"US",
+           "company_id":3526,
+           "department_id":7084,
+           "recruiter_id":6043,
+           "owner_id":9988,
+           "category_name":"",
+           "is_hot":true,
+           "start_date":"2019-03-07T19:39:31.693Z",
+           "salary":"",
+           "max_rate":"",
+           "duration":"",
+           "type":"",
+           "openings":0,
+           "external_id":"",
+           "description":"",
+           "notes":"",
+           "contact_id":8994,
+           "workflow_id":3712,
+           "custom_fields":[
+              {
+                 "id":8373,
+                 "value":"lorem"
+              }
+           ]
         }');
 
         echo '############################################################<br />';
         print_r($output);
         echo '############################################################<br />';
-
-
 
         return response()->json([
             'status' => 'success',
