320db407d79b132d4b50a8e9e1c533c94e3883aa
Kai Hoa <WI4IYdz!15626>
2022-04-28 10:52:47 +0200

    versie 3.5

--- app/Services/GeonamesClient.php
+++ app/Services/GeonamesClient.php
@@ -7,6 +7,10 @@ class GeonamesClient
 {
     public function getProvinceFromPostalCode($postalCode)
     {
+        if (!preg_match("/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i", $postalCode)) {
+            return 'VOER PROVINCIE IN';
+        }
+
         $response = RequestClient::get(
             sprintf(config('services.geonames.postalcode_endpoint'), str_replace(' ', '', $postalCode))
         );
